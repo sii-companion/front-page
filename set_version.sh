427153fa47b99d08739880834fc965ead0fe7221
Will Haese-Hill <wahhill@gmail.com>
2023-03-16 16:29:04 +0000

    shell script to set version number from latest release

---
+++ set_version.sh
@@ -0,0 +1,8 @@
+github_token=$(<"$1")
+
+curl -L \
+ -H "Accept: application/vnd.github+json" \
+ -H "Authorization: Bearer $github_token" \
+ -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/iii-companion/companion/releases/latest \
+  | json tag_name > dist/version.txt
